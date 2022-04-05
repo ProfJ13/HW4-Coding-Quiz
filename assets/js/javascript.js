123276ffbaa08c60156dbc61a43b2d9e486ed55e
Ron Crawford <roncrawford1300@gmail.com>
2022-04-05 13:27:56 -0400

    Initial CSS and functions

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,3 +4,26 @@ var questionsEl = document.getElementById("questions");
 var optionsEl = document.getElementById("options");
 var submitBtn = document.getElementById("submit");
 var initialsEl = document.getElementById("initials");
+var time = questions.length * 10;
+var timerId;
+
+function beginQuiz() {
+    var landingPage = document.getElementById("landing-page");
+    landingPage.setAttribute("class", "landing hide");
+  
+    questionsEl.setAttribute("class", " ");
+
+    timerId = setInterval(function(){
+      countdown();
+    }, 1000);
+
+    timerEl.textContent = time;
+  
+  }
+  
+  function countdown() {
+    timerEl.textContent = time;
+    time--;
+  
+  }
+  
