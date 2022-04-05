7edb1653f846f68c8ae763767d2d858cf0a6c95c
Ron Crawford <roncrawford1300@gmail.com>
2022-04-05 12:36:43 -0400

    HTML and initial variables

---
+++ assets/js/javascript.js
@@ -0,0 +1,6 @@
+var timerEl = document.getElementById("timer");
+var startBtn = document.getElementById("start");
+var questionsEl = document.getElementById("questions");
+var optionsEl = document.getElementById("options");
+var submitBtn = document.getElementById("submit");
+var initialsEl = document.getElementById("initials");
